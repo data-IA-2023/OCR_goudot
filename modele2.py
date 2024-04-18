2717935938540b6e47f49d16c7047e705e926b68
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2024-04-18 14:58:31 +0200

    UPD modele2

--- modele2.py
+++ modele2.py
@@ -1,10 +1,21 @@
+import pyodbc
 from sqlalchemy import create_engine, Column, DateTime, ForeignKey, Integer, String, Float, Date, select, update, delete, func
 from sqlalchemy.orm import relationship, sessionmaker, Session, mapped_column, declarative_base
 import os, dotenv, requests, datetime, json, math, subprocess, re, glob
+from colorama import Fore, Back, Style
 
 dotenv.load_dotenv()
 DATABASE_URL=os.getenv('DATABASE_URL')
 DISCORD=os.getenv('DISCORD_WEBHOOK')
+
+OCR_API=os.getenv('OCR_API')
+VISION_KEY=os.getenv('VISION_KEY')
+VISION_ENDPOINT=os.getenv('VISION_ENDPOINT')
+VISION_URL=f"{VISION_ENDPOINT}/computervision/imageanalysis:analyze?features=caption,read&model-version=latest&language=en&api-version=2024-02-01"
+headers = {'Accept': 'application/json'
+    , 'Ocp-Apim-Subscription-Key': VISION_KEY
+    , 'Content-Type': 'application/json'}
+
 # Connection à la BDD
 engine = create_engine(DATABASE_URL) # , echo=True
 # classe de base dont no objets ORM vont dériver
@@ -24,7 +35,7 @@ class Client(Base):
 
 class Facture(Base):
     __tablename__ = 'factures'
-    no = Column(String, primary_key=True)
+    no = Column(String(25), primary_key=True)
     dt = Column(DateTime)
     total = Column(Float)
     cumul = Column(Float)
@@ -35,11 +46,31 @@ class Facture(Base):
 
     txt_ocr = Column(String)
     txt_qr = Column(String)
+    txt_vision = Column(String)
 
     commandes = relationship("Commande", order_by="Commande.no", cascade="delete") # , back_populates="factures"
 
     def __str__(this):
         return f"FACTURE [{this.no}] {this.total}€"
+
+    def update_vision(this):
+        # Récupération des données
+        image_url=OCR_API+"/"+this.no
+
+        try:
+            r = requests.post(VISION_URL, headers=headers, json={'url': image_url})
+            if r.status_code==200:
+                txt_vision=""
+                for line in r.json()['readResult']['blocks'][0]['lines']:
+                    bp=line['boundingPolygon'][0]
+                    txt_vision+=f"{bp['x']}x{bp['y']} {line['text']}\n"
+                    #print(f"    Line {bp['x']}x{bp['y']} : {line['text']}")
+                this.txt_vision=txt_vision
+            else:
+                this.txt_vision = f"Status {r.status_code}, VISION_URL={VISION_URL}, image_url={image_url}"
+        except Exception as e:
+            this.txt_vision = f"Exception {e}..."
+
 
     # méthode de classe (avec @staticmethod)
     @staticmethod
@@ -51,12 +82,15 @@ class Facture(Base):
         False sinon
         '''
         with Session(engine) as session:
-            query = select(Facture).where(Facture.no==no)
-            session.execute(delete(Facture).where(Facture.no==no)) # supprime la facture...
-            session.execute(delete(Commande).where(Commande.facture_id==no)) # supprime les commandes...
-            res = session.execute(query).scalar()
+            #query = select(Facture).where(Facture.no==no)
+            fac = session.get(Facture, no)
+            if fac.total!=fac.cumul:
+                session.execute(delete(Facture).where(Facture.no==no)) # supprime la facture...
+                session.execute(delete(Commande).where(Commande.facture_id==no)) # supprime les commandes...
+            #res = session.execute(query).scalar()
             # Si la facture n'existe pas encore:
-            if not res:
+            if fac.total!=fac.cumul:
+                print('---------------------------------------------------------')
                 print(f'Read {no}')
                 if DISCORD:
                     requests.post(DISCORD, json = {"content": f"Facture.read_file('{no}')"})
@@ -80,7 +114,7 @@ class Facture(Base):
                         txt_ocr+=line
                         line=line.strip()
                         mtot=re.match(r'TOTAL ([0-9.,]+)', line)
-                        mcmd=re.match(r'(.*)\. ([0-9B]+) *x *([0-9.,]+)', line)
+                        mcmd=re.match(r'(.*)\. ([0-9BiTSo]+)[ «.]*x[ «]*([0-9.,]+)', line)
                         if not line: pass
                         elif 'Bill to' in line: x['cust_name']=line[8:]
                         elif 'INVOICE' in line: pass
@@ -88,22 +122,23 @@ class Facture(Base):
                         elif mtot: x['total']=round(float(mtot.group(1).replace(',', '.')), 2)
                         elif mcmd:
                             qty=mcmd.group(2)
+                            if qty=='S': qty='5'
                             if qty=='B': qty='8'
+                            if qty=='o': qty='9'
+                            if qty=='i': qty='1'
+                            if qty=='T': qty='7'
                             cmd={'name': mcmd.group(1), 'qty': int(qty), 'price': round(float(mcmd.group(3).replace(',', '.')), 2)}
                             cmds.append(cmd)
                             cumul += cmd['qty'] * cmd['price']
                         else:
                             x['cust_adr']+=line
-                            print('***', line)
+                            print(Fore.YELLOW, '***', line, Style.RESET_ALL)
 
-                #print(txt_qr)
-                print('---------------------------------------------------------')
-
-                #print(txt_ocr)
-                print(x, cmds)
                 cumul=round(cumul, 2)
-                print(x['total'], cumul)
                 x['cust_adr']=x['cust_adr'].replace('Address ', '')
+                if cumul!=x['total']:
+                    print(x, cmds)
+                    print(Fore.RED, '*** FACTURE total/cumul', x['total'], cumul, Style.RESET_ALL)
 
                 client = session.get(Client, x['cust_id']) # session.execute(select(Client).where(Client.id==x['cust_id'])).scalar()
                 if not client:
@@ -122,13 +157,13 @@ class Facture(Base):
                     session.add(cmd)
                 session.add(fac)
                 session.commit()
-                return True
+                return fac.total==fac.cumul
             #return fac # facture créee à partir des info des TXT
 
 class Produit(Base):
     __tablename__ = 'produits'
     #__table_args__ = {'schema': 'test_goudot'}
-    name = Column(String, primary_key=True, index=True)
+    name = Column(String(200), primary_key=True, index=True)
     price = Column(Float)
     #factures = relationship("Facture", back_populates="client")
 
@@ -158,18 +193,15 @@ Base.metadata.create_all(bind=engine)
 
 if __name__=="__main__":
 
-    with Session(engine) as session:
-        client = Client(id=-1, name='Essai', adr='Ici', cat='X')
-        print(str(client))
+    #Facture.update_vision("FAC_2019_0003-4174848")
 
-    quit()
+    fns=glob.glob('static/*.pngqr.txt')
 
-    fns=glob.glob('static/*.txt')
-    fns=[fn[7:-10] for fn in fns if '.pngqr.txt' in fn]
-    #print(fns)
-
-    for no in fns[:10000]:
-        fac = Facture.read_file(no) # "FAC_2019_0003-4174848"
+    for no in [fn[7:-10] for fn in fns]:
+        flag = Facture.read_file(no) # "FAC_2019_0003-4174848"
+        if not flag:
+            #Facture.update_vision(no)
+            pass
     #print(fac)
     #print('DATABASE_URL=', DATABASE_URL)
     with Session(engine) as session:
